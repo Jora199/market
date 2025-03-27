6637847e4db1723fa3555482a853a2e564c991c6
Jora199 <gumba22face@gmail.com>
2025-03-27 12:43:30 +0500

    Update data - 2025-03-27 12:43:30

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def main():
     df, _ = load_data()
     supply_dict = load_supply_data()
     img_dict = load_image_data()
-    default_img = "https://ibb.co/Qv84JNZY"
+    default_img = "https://i.ibb.co/tBNqn8G/no-image.png"
 
     # Заголовок
     st.title("Price History Analysis")
